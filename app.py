7222f765c00236bce617b185f047278de5e9a432
Varun Jain <varun.m.jain84@gmail.com>
2019-01-09 15:54:14 +1100

    Learning Python

---
+++ app.py
@@ -0,0 +1,14 @@
+char_name = "Jason"
+char_age = "35"
+is_male = True
+print("***********************************************************************************************")
+print("Hi my name is " + char_name + ",")
+print(char_name + " Is nice boy"",")
+print(char_name + " is " + char_age + " now,")
+char_name = "Tarzan"
+char_name1 = "Hulk"
+print(char_name + " is getting younger with " + char_age)
+print(char_name + " will be confident and fearless soon!")
+print(char_name + " will be best until " + char_age + " age")
+print("This is a surprise which is coming by end of the year for " + char_name1 + " at age " +char_age)
+print("************************************************************************************************")
